f7f0e264f93cbba41582d3795ab47e186bcefaea
Luiscordeiro <luiscordeiro2006@gmail.com>
2025-01-14 20:23:04 -0300

    pdf full

--- src/pdf/pdf.controller.ts
+++ src/pdf/pdf.controller.ts
@@ -10,10 +10,12 @@ export class pdfController {
   async getProductPdf(@Param('id') productId: string, @Res() res: Response) {
     try {
       const pdfData = await this.pdfService.generateProductPdf(productId);
-      res.set({
-        'Content-Type': 'application/pdf', 
-        'Content-Disposition': `attachment; filename=aluguel-${productId}.pdf`, 
-      });
+      const fileName = `relatorio_produtos_${new Date().toISOString().split('T')[0]}.pdf`;
+
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': `attachment; filename=${fileName}`,
+    });
 
       res.send(pdfData);
     } catch (error) {
@@ -40,7 +42,84 @@ async getAllProductPdf(@Res() res: Response) {
   } catch (error) {
     console.error('Erro ao gerar PDF:', error.message);
     res.status(500).send({ error: error.message });
-  }
-}
+  }}
+// =-=-=-=-=-=-=-=-=-=-=-==-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+  @Get('client/:id')
+  async getClientPdf(@Param('id') productId: string, @Res() res: Response) {
+    try {
+      const pdfData = await this.pdfService.generateClientPdf(productId);
+      const fileName = `relatorio_clientes_${new Date().toISOString().split('T')[0]}.pdf`;
+
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': `attachment; filename=${fileName}`,
+    });
+
+      res.send(pdfData);
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
+  }
+  @Get('client')
+async getAllClientPdf(@Res() res: Response) {
+  try {
+    const pdfData = await this.pdfService.generateAllClientPdf();
+
+    if (pdfData.length === 0) {
+      throw new Error('O PDF gerado está vazio.');
+    }
+
+    const fileName = `relatorio_clientes_${new Date().toISOString().split('T')[0]}.pdf`;
+
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': `attachment; filename=${fileName}`,
+    });
+
+    res.send(pdfData); 
+  } catch (error) {
+    console.error('Erro ao gerar PDF:', error.message);
+    res.status(500).send({ error: error.message });
+  }}
+
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+  @Get('employee/:id')
+  async getEmployeePdf(@Param('id') employeeId: string, @Res() res: Response) {
+    try {
+      const pdfData = await this.pdfService.generateEmployeePdf(employeeId);
+      const fileName = `relatorio_funcionarios_${new Date().toISOString().split('T')[0]}.pdf`;
+
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': `attachment; filename=${fileName}`,
+    });
+
+      res.send(pdfData);
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
+  }
+  @Get('employee')
+async getAllEmployeePdf(@Res() res: Response) {
+  try {
+    const pdfData = await this.pdfService.generateAllEmployeePdf();
+
+    if (pdfData.length === 0) {
+      throw new Error('O PDF gerado está vazio.');
+    }
+
+    const fileName = `relatorio_funcionarios_${new Date().toISOString().split('T')[0]}.pdf`;
+
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': `attachment; filename=${fileName}`,
+    });
+
+    res.send(pdfData); 
+  } catch (error) {
+    console.error('Erro ao gerar PDF:', error.message);
+    res.status(500).send({ error: error.message });
+  }}
 }
--- src/pdf/pdf.service.ts
+++ src/pdf/pdf.service.ts
@@ -3,6 +3,7 @@ import * as puppeteer from 'puppeteer';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as fs from 'fs';
 import * as path from 'path';
+import { error } from 'console';
 
 @Injectable()
 export class PdfService {
@@ -106,6 +107,159 @@ export class PdfService {
     return Buffer.from(pdfBuffer);
   }
   
+  async generateClientPdf(clientId: string): Promise<Buffer> {
+    const client = await this.prisma.client.findUnique({
+      where: {id: clientId},
+    });
+
+    if(!client) {
+      throw new Error('Cliente não encontrado')
+    }
+
+    const templatePath = path.join(__dirname, '../../dist/pdf/templates/Cliente', 'client-template.html');
+    const cssPath = path.join(__dirname, '../../dist/pdf/templates/Cliente', 'client-styles.css');
+
+    if (!fs.existsSync(templatePath) || !fs.existsSync(cssPath)) {
+      throw new Error('Template or CSS not found');
+    }
+
+    let templateHtml = fs.readFileSync(templatePath, 'utf-8');
+    const stylesCss = fs.readFileSync(cssPath, 'utf-8');
+
+    templateHtml = templateHtml
+    .replace('{{name}}', client.name)
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(templateHtml, { waitUntil: 'networkidle0' });
+    await page.addStyleTag({ content: stylesCss });
+    const pdfBuffer = await page.pdf({ format: 'A4' });
+
+    await browser.close();
+
+    return Buffer.from(pdfBuffer);
+  }
+
+  async generateAllClientPdf(): Promise<Buffer> {
+    const client = await this.prisma.client.findMany();
   
+    if (!client || client.length === 0) {
+      throw new Error('Nenhum cliente encontrado');
+    }
   
+    const fs = require('fs');
+    const path = require('path');
+    const templatePath = path.join(__dirname, '../../dist/pdf/templates/Cliente', 'allClient-template.html');
+    const cssPath = path.join(__dirname, '../../dist/pdf/templates/Cliente', 'allClient-styles.css');
+  
+    if (!fs.existsSync(templatePath) || !fs.existsSync(cssPath)) {
+      throw new Error('Template ou CSS não encontrados');
+    }
+  
+    let templateHtml = fs.readFileSync(templatePath, 'utf-8');
+    const stylesCss = fs.readFileSync(cssPath, 'utf-8');
+
+    let clientsHtml = '';
+    client.forEach((client) => {
+      clientsHtml += `
+        <div class="product">
+          <h2>${client.name}</h2>
+        </div>
+      `;
+    });
+  
+    templateHtml = templateHtml.replace('{{client}}', clientsHtml);
+  
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(templateHtml, { waitUntil: 'networkidle0' });
+    await page.addStyleTag({ content: stylesCss });
+    const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
+  
+    if (pdfBuffer.length === 0) {
+      throw new Error('O PDF gerado está vazio.');
+    }
+  
+    await browser.close();
+    return Buffer.from(pdfBuffer);
+  }
+
+  // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  
+  async generateEmployeePdf(employeeId: string): Promise<Buffer> {
+    const employee = await this.prisma.employee.findUnique({
+      where: {id: employeeId},
+    });
+
+    if(!employee) {
+      throw new Error('Funcionário não encontrado')
+    }
+
+    const templatePath = path.join(__dirname, '../../dist/pdf/templates/Employee', 'Employee-template.html');
+    const cssPath = path.join(__dirname, '../../dist/pdf/templates/Employee', 'Employee-styles.css');
+
+    if (!fs.existsSync(templatePath) || !fs.existsSync(cssPath)) {
+      throw new Error('Template or CSS not found');
+    }
+
+    let templateHtml = fs.readFileSync(templatePath, 'utf-8');
+    const stylesCss = fs.readFileSync(cssPath, 'utf-8');
+
+    templateHtml = templateHtml
+    .replace('{{name}}', employee.name)
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(templateHtml, { waitUntil: 'networkidle0' });
+    await page.addStyleTag({ content: stylesCss });
+    const pdfBuffer = await page.pdf({ format: 'A4' });
+
+    await browser.close();
+
+    return Buffer.from(pdfBuffer);
+  }
+
+  async generateAllEmployeePdf(): Promise<Buffer> {
+    const employee = await this.prisma.employee.findMany();
+  
+    if (!employee || employee.length === 0) {
+      throw new Error('Nenhum cliente encontrado');
+    }
+  
+    const fs = require('fs');
+    const path = require('path');
+    const templatePath = path.join(__dirname, '../../dist/pdf/templates/Employee', 'allEmployee-template.html');
+    const cssPath = path.join(__dirname, '../../dist/pdf/templates/Employee', 'allEmployee-styles.css');
+  
+    if (!fs.existsSync(templatePath) || !fs.existsSync(cssPath)) {
+      throw new Error('Template ou CSS não encontrados');
+    }
+  
+    let templateHtml = fs.readFileSync(templatePath, 'utf-8');
+    const stylesCss = fs.readFileSync(cssPath, 'utf-8');
+
+    let employeeHtml = '';
+    employee.forEach((employee) => {
+      employeeHtml += `
+        <div class="product">
+          <h2>${employee.name}</h2>
+        </div>
+      `;
+    });
+  
+    templateHtml = templateHtml.replace('{{employee}}', employeeHtml);
+  
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(templateHtml, { waitUntil: 'networkidle0' });
+    await page.addStyleTag({ content: stylesCss });
+    const pdfBuffer = await page.pdf({ format: 'A4', printBackground: true });
+  
+    if (pdfBuffer.length === 0) {
+      throw new Error('O PDF gerado está vazio.');
+    }
+  
+    await browser.close();
+    return Buffer.from(pdfBuffer);
+  }
 }
